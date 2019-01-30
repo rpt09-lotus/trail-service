5daecb7a14e6df5392d9cfac398ead2e99a7e504
Ryan Brennan <ryanbrennanbc1@gmail.com>
2019-01-29 16:56:46 -0800

    [wip] cleaned up for trails

--- database-mongo/mongoSeed.js
+++ database-mongo/mongoSeed.js
@@ -13,3 +13,82 @@ mongoose.connect('mongodb://localhost/trailsService')
 const trailTags = ['dogs on leash','hiking','mountain biking','trail walking','views','wildlife','muddy','dog friendly','backpacking','birding','camping','nature trips','paddle sports',
 'beach','cave','forest','river','waterfall','bugs','historic site','wildlife','rocky','kid friendly','no shade','wheelchair friendly','wild flowers','lake','paved','road biking'];
 
+let tagArray = []
+
+Trail.deleteMany({}, (err) => {
+  console.log('collection removed');
+
+  // Batch configuration
+  let batchLimit = 5000;
+  let batch = 1;
+
+  let trailsBatch = [];
+
+
+  const createTrail = (trailId) => {
+
+    let trail = new Trail({
+      trail_id: i,
+      trail_name: seedFuncs.setTrailName(),
+      distance: seedFuncs.setNumber(100),
+      distance_units: 'miles',
+      elevation_gain: seedFuncs.setNumber(1900),
+      elevation_units: 'ft',
+      description: seedFuncs.sentences(3, 5, 200),
+      route_type: seedFuncs.setRouteType(),
+      difficulty: seedFuncs.setDifficulty(),
+      tags: seedFuncs.tags(),
+      general_area: seedFuncs.sentences(1, 3, 65),
+      origin: seedFuncs.setOrigin()
+    });
+    trailsBatch.push(trail);
+  };
+
+  const batchSaver = () => {
+    return new Promise ((resolve, reject) => {
+      Photo.insertMany(photosBatch, (err, docs) => {
+        if (err) {
+          reject('error occured in inserting to db: ', err);
+        } else {
+          resolve();
+        }
+      });
+    });
+  };
+
+
+  let records = 1000000;
+
+  const createRecords = async () => {
+
+  for (i = records; i > 0; i--) {
+    createTrail(i);
+    if (trailsBatch.length === batchLimit) {
+      await batchSaver();
+      trailsBatch.length = 0;
+
+    }
+  }
+  console.log(`Completed saving ${records} primary records to DB`);
+};
+  createRecords();
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+  // Batch saver
+
+
+
+
+
